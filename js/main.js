fbeda4003cacad79d2ae1188bda564427ad978d5
ourcompan1 <ourcompany39@gmail.com>
2024-02-08 20:18:55 +0300

    main page
    Сделал главную страницу

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper", {  
+   effect: "fade",
     pagination: {
       el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
     },
   });
